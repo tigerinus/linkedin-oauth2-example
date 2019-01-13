8a6047148eb457ca3d3338fe07d0230c1f6ac60f
Tiger Wang <tigerwang@outlook.com>
2019-01-13 13:44:22 -0500

    get basicProfile using token

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const request = require('request');
 const app = express();
 const port = 4000;
 
+const getPerson = (accessToken, next) => {
+    request.get("https://api.linkedin.com/v1/people/~?format=json", {
+        'auth': {
+            'bearer': accessToken
+        }
+    }, next);
+};
+
+const renderPDF = (res) => (error, r, body) => {
+    if (error) {
+        res.status(500).send(error);
+    }
+
+    var person = JSON.parse(body);
+
+    var result = `
+        <html>
+            <h1>${person.firstName} ${person.lastName}</h1>
+            <p>${person.headline}</p>
+        </html>
+    `
+
+    res.status(200).send(result);
+};
+
 app.use(express.static('public'));
-
-
 app.get("/auth", (req, res) => {
     var code = req.query['code'];
     var client_id = req.query['state']; // using state value as client_id, to know which app we are dealing with.
@@ -24,7 +47,9 @@ app.get("/auth", (req, res) => {
             return;
         }
         
-        res.status(200).json(body);
+        var accessToken = JSON.parse(body).access_token;
+
+        getPerson(accessToken, renderPDF(res));
     });
 });
 
